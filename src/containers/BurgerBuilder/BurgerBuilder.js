676512b357934545259834f9a9f455787ab961c7
Meghna Srivastava <meghna.s1906@gmail.com>
2018-03-19 16:31:07 +0100

    rendering burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component, Fragment } from 'react';
 // import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render() {
         return (
             <Fragment>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </Fragment>
         );
