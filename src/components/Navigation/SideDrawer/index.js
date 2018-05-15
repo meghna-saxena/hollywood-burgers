4ad168f47bc33927817893f17a7447de0f1d4fce
Meghna Srivastava <meghna.s1906@gmail.com>
2018-05-15 17:46:17 +0200

    reused backdrop with sidedrawer for mobile devices

--- src/components/Navigation/SideDrawer/index.js
+++ src/components/Navigation/SideDrawer/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Logo from '../../Logo';
 import NavigationItems from '../NavigationItems';
+import Backdrop from '../../UI/Backdrop';
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Fragment>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Fragment>
     );
 }
 
