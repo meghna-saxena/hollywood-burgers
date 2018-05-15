0f0855891e7aacbc08a6d036dfaa3b87634ab524
Meghna Srivastava <meghna.s1906@gmail.com>
2018-05-15 12:48:20 +0200

    debugged missing variable

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -3,23 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient';
 
 const burger = (props) => {
-    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            // console.log('[new array]', [...Array(props.ingredients[igKey])]);
-            // console.log('igkey', igKey);
-            // console.log('value of i' + i);
-            return <BurgerIngredient key={igKey + i} type={igKey} />
-        });
-    })
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        })
+        //Flatten the array
+        .reduce((arr, el) => {
+            // return arr.concat(el)
+            return [...arr, ...el]
+        }, []);
 
-    //Flatten the array
-    .reduce((arr, el) => {
-        // return arr.concat(el)
-        return [...arr, ...el]
-    }, []);
-
-    console.log('transformedIngredients', transformedIngredients); 
-    //Earlier: [Array(1), Array(1), Array(2), Array(2)] && now: transformedIngredients []
+    // console.log('transformedIngredients', transformedIngredients); 
+    // //Earlier: [Array(1), Array(1), Array(2), Array(2)] && now: transformedIngredients []
 
     if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please start adding ingredients!</p>
